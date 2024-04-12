8135d6e86a9d78230c2d6ce58be9c9df810740fa
iitapanderson <114035053+iitapanderson@users.noreply.github.com>
2024-04-12 10:10:59 +1000

    a

---
+++ function Write-Log.ps1
@@ -0,0 +1,45 @@
+<#
+.SYNOPSIS
+    Creates a log file and writes log messages to it.
+
+.DESCRIPTION
+    This script creates a log directory if it doesn't exist and defines the log file path based on the current date and time. 
+    It also contains a function called Write-Log that takes a log message as input and writes it to the log file.
+
+.PARAMETER Message
+    The log message to be written to the log file.
+
+.EXAMPLE
+    Write-Log -Message "This is a sample log message"
+
+    This example demonstrates how to use the Write-Log function to write a log message to the log file.
+
+.NOTES
+    Author: Phillip Anderson
+    Date:   10/04/2024
+#>
+
+# Create the log directory if it doesn't exist
+$logDirectory = "C:\Support"
+if (-not (Test-Path -Path $logDirectory)) {
+    New-Item -ItemType Directory -Path $logDirectory | Out-Null
+}
+
+# Define the log file path
+$logFile = Join-Path -Path $logDirectory -ChildPath "$(Get-Date -Format 'dd.MM.yyyy.HH.mm.ss')-log.txt"
+
+# Function to write log messages
+function Write-Log {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Message
+    )
+    
+    $timestamp = Get-Date -Format "dd-MM-yyyy HH:mm:ss"
+    $logMessage = "$timestamp - $Message"
+    
+    Add-Content -Path $logFile -Value $logMessage
+    Write-Output $logMessage
+}
+
+. "$(Join-Path -Path 'https://raw.githubusercontent.com/integrateitaus/Powershell' -ChildPath 'Write-Log.ps1')"
